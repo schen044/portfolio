2063983bbd79619b8c61ca48add153b1b2aff5a2
schen044 <ninjarulers@yahoo.com>
2022-11-15 15:50:32 -0800

    MERN-Stack Infrastructure

---
+++ routes/api/users.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const usersCtrl = require('../../controllers/api/users')
+// require the authorization middleware function
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
+
+router.post('/', usersCtrl.create)
+router.post('/login', usersCtrl.login)
+// Insert ensureLoggedIn on all routes that need protecting
+router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
+
+module.exports = router
