f25fbb53bc0f55066aa3a41d8e8f64b1cc2092b6
schen044 <ninjarulers@yahoo.com>
2022-12-06 23:25:58 -0800

    Adds home page

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,17 @@
+import Projects from '../../components/Projects/Projects'
+import './HomePage.css'
+
+export default function AboutPage() {
+
+  return (
+    <main className="home">
+      <div>
+        <h1>Hi, I'm Serra!</h1>
+        <p>Brand statement</p>
+      </div>
+      <div>
+        <h2>My Projects:</h2>
+        <Projects />
+      </div>
+      </main>
+      )}
