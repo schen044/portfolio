cc832d9d3d0735be780c41e697d13d1f6dac2bcd
schen044 <ninjarulers@yahoo.com>
2022-12-13 13:09:56 -0800

    Adds resume link

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -9,6 +9,7 @@ export default function AboutPage() {
       <div className="about">
         <h1>Hi, I'm Serra!</h1>
         <p>I am a full-stack developer who loves solving puzzles and tinkering. Whether it’s building a 3D printed keyboard or trying to understand how someone cut down loading times for a game, I am always seeing how software engineering fits into my everyday life. In marrying the two together, I am able to remain passionate not just about my hobbies, but my work as well.</p>
+        <a href="https://docs.google.com/document/d/1caACGWkcmfQG2KfIE4sOUe5mgvh8myjuYiz5KxWIP5Y/export?format=pdf"><button>⤓ My Resume</button></a>
       </div>
       <div>
         <h2>My Projects:</h2>
